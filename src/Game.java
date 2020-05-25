7c9d5ebc44e5c54aa0265fd78cc3bbb58aff9599
Mr. Bianchi <MisterBianchi@gmail.com>
2020-05-25 17:36:02 -0400

    Initial Project files -JAB

---
+++ src/Game.java
@@ -0,0 +1,89 @@
+public class Game {
+
+  private Grid grid;
+  private int userRow;
+  private int msElapsed;
+  private int timesGet;
+  private int timesAvoid;
+  
+  public Game() {
+
+    grid = new Grid(5, 10);
+    userRow = 3;
+    msElapsed = 0;
+    timesGet = 0;
+    timesAvoid = 0;
+    updateTitle();
+    grid.setImage(new Location(userRow, 0), "user.gif");
+  }
+  
+  public void play() {
+
+    while (!isGameOver()) {
+      grid.pause(100);
+      handleKeyPress();
+      if (msElapsed % 300 == 0) {
+        scrollLeft();
+        populateRightEdge();
+      }
+      updateTitle();
+      msElapsed += 100;
+    }
+  }
+  
+  public void handleKeyPress(){
+
+    //check last key pressed
+    int key = grid.checkLastKeyPressed();
+    System.out.println(key);
+
+    //set "w" key to move the plane up
+    if(key == 87){
+        //check case where out of bounds
+
+        //change the field for userrow
+
+        userRow--;
+
+        //shift the user picture up in the array
+        Location loc = new Location(userRow, 0);
+        grid.setImage(loc, "user.gif");
+        
+        Location oldLoc = new Location(userRow+1, 0);
+        grid.setImage(oldLoc, null);
+
+  }
+    //if I push down arrow, then plane goes down
+
+
+  }
+  
+  public void populateRightEdge(){
+
+  }
+  
+  public void scrollLeft(){
+
+  }
+  
+  public void handleCollision(Location loc) {
+
+  }
+  
+  public int getScore() {
+    return 0;
+  }
+  
+  public void updateTitle() {
+    grid.setTitle("Game:  " + getScore());
+  }
+  
+  public boolean isGameOver() {
+    return false;
+  }
+    
+  public static void main(String[] args) {
+    Game game = new Game();
+    game.play();   
+  }
+}
